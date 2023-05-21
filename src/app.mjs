3188580f0f3899d1e00a183bdfe65092da4974c9
Liya Vais <liyavais1408@gmail.com>
2023-05-21 23:51:49 +0600

    new

--- src/app.mjs
+++ src/app.mjs
@@ -3,9 +3,6 @@ import { initialCards } from './data/cards-list.mjs'
 // import { inputValidationHandler } from './data/validation.mjs'
 import { editProfileInputsValidation } from './data/validation2.mjs'
 
-import { initializeApp } from '../node_modules/firebase/app'
-
-console.log(initializeApp)
 
 const buttonNewImage = document.querySelector('.profile__add-new-image-button')
 const popUpGroup = document.querySelector('.popup-group')
@@ -61,19 +58,34 @@ addNewImageButton.onclick = (evt) => {
     const newObject = {
         name: placeName.value,
         link: linkByUser.value
-      }
-      console.log(newObject)
+    }
+    console.log(newObject)
     initialCards.push(newObject)
     renderCardsList()
     closeAllPopups()
     addNewImagePopup.querySelector('form').reset()
-    
+
 }
 
 // window.addEventListener('DOMContentLoaded', inputValidationHandler)
 window.addEventListener('DOMContentLoaded', renderCardsList)
 window.addEventListener('DOMContentLoaded', editProfileInputsValidation)
 
+
+const getData = async (url) => {
+    const res = await fetch(url)
+    const json = await res.json()
+    return json
+}
+
+const url = 'https://api.thecatapi.com/v1/images/search?limit=10'
+
+try {
+    const dataCat = await getData(url)
+    console.log(dataCat)
+} catch (error) {
+    console.log(error.message)
+}
 
 
 
